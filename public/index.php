eea351508ac3bf2b7fa26403df15b94a879352c0
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2021-11-29 20:01:49 +0100

    add current file name in title

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu Maker</title>
+    <title>Menu Maker - <?=$_SERVER['PHP_SELF']?></title>
     <style>
         a.active {
             color: red;
@@ -15,9 +15,9 @@
 </head>
 <body>
     <nav>
-        <?php 
+        <?php
         $pages = [
-            ["name" => "Home", "file" => "/index.php"], 
+            ["name" => "Home", "file" => "/index.php"],
             ["name" => "About us", "file" => "/about.php"],
             ["name" => "Contact", "file" => "/contact.php"],
         ];
